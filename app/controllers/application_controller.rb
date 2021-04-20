716997ced574edfc0ad69a7b36b4caac46ff005d
amongiove <amandamongiove@gmail.com>
2021-04-20 10:19:57 -0500

    commit tree ba6af0a8521a81fdb89b5e4d5eaa598140f136f1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-end
+
+#   def root_path
+#     puts "enter loop"
+#     puts session[:username]
+#     if !session[:username]
+#       redirect_to '/login'
+#     else
+#       redirect_to '/'
+#     end
+#   end
+
+# end
+
+# helpers do
+#   def logged_in?
+#     !!session[:user_id]
+#   end
+
+#   def current_user
+#     User.find(session[:user_id])
+  end
